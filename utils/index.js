0084fa8d7f01383c0fd91b55233eac4d77669df8
fenglingnan <185975315@qq.com>
2021-01-19 23:13:30 +0800

    koa起步

---
+++ utils/index.js
@@ -0,0 +1,36 @@
+const list=require('./white')
+const {client}=require('../model/redis_mod/index')
+function res(){
+    return async (ctx,next)=>{
+        ctx.get_key=function (){
+            return new Promise(((resolve, reject) => {
+                client.hgetall(ctx.header.token,function (err,val){
+                    if(!list.includes(ctx.url)){
+                        if(err){
+                            ctx.body = {
+                                code:'501',
+                                msg:'token错误',
+                                data:null
+                            }
+                            reject(err)
+                        }else{
+                            resolve(val)
+                        }
+                    }
+                })
+            }))
+        }
+
+
+        ctx.back=function (data,msg,code){
+            let obj={
+                code:code||'200',
+                data:data,
+                msg:msg||'操作成功.'
+            }
+            return obj
+        }
+        await next()
+    }
+}
+module.exports=res
